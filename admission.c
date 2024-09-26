c108f39f8a5af26d91c0c6edac09bb8a5bdb319a
randhir1209 <168509516+randhir1209@users.noreply.github.com>
2024-09-26 21:13:46 +0530

    Add files via upload

---
+++ admission.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+    int phy,che,maths;
+    printf("Enter maths marks:");
+    scanf("%d",&maths);
+    printf("Enter phy marks:");
+    scanf("%d",&phy);
+    printf("Enter che marks:");
+    scanf("%d",&che);
+    if(maths>=65 && phy>=55 && che>=50 && maths+phy+che>=180 ||maths+phy >=140)
+    {
+        printf("Students are eligible.");
+    }
+    else
+    {
+        printf("Students are not eligible.");
+    }
+    return;
+}
