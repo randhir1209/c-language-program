fc1c58b725258244b7aff5476c01cf876accc73b
randhir1209 <168509516+randhir1209@users.noreply.github.com>
2024-09-29 13:58:46 +0530

    Add files via upload

---
+++ size.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    char ch;
+    printf("size of integer=%d\n size of float=%d\n size of char=%d\n",sizeof(a),sizeof(b),sizeof(ch));
+    return 0;
+}
